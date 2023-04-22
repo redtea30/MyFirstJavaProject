8ee5e68941306e3324ab54f0846e7ba36daae067
jkspstyu <jkspstyu@gmail.com>
2023-04-22 21:58:43 +0800

    learn how to use IDEA

--- Chapter05/src/Arraytest.java
+++ Chapter05/src/Arraytest.java
@@ -1,7 +1,6 @@
-class ArrayPractical {
-    public static void main(String[] args) {
-//创建字符串
-/*        数据类型[] 数组名 = new 数据类型[]{元素1,元素2,元素3,...};
+class Arraytest {
+    public static void main(String[] args){
+        /*        数据类型[] 数组名 = new 数据类型[]{元素1,元素2,元素3,...};
 
         或
 
@@ -11,8 +10,8 @@ class ArrayPractical {
         int[] arr = new int[5];
         System.out.println(arr);
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+            System.out.println(arr[i]);}
+
 
 
     }
