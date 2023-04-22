388ce4d337e42a73c7097b8ba7ef90289b60ccc7
jkspstyu <jkspstyu@gmail.com>
2023-04-22 23:36:43 +0800

    array practice

--- Chapter05/src/Arraytest.java
+++ Chapter05/src/Arraytest.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Arraytest {
     public static void main(String[] args) {
         /*        数据类型[] 数组名 = new 数据类型[]{元素1,元素2,元素3,...};
@@ -30,13 +32,68 @@ class Arraytest {
             System.out.print(arr2[i] + "\n");
         }
 
+        //set each element in arr2 to arr3
         int[] arr3 = new int[arr2.length];
         for (int i = 0; i < arr2.length; i++) {
             arr3[i] = arr2[i];
         }
-        arr3[2] = 5;
-        System.out.println(arr3[2]);
-        System.out.println(arr2[2]);
+        arr3[2] = 5;//prove this way works
+        System.out.println(arr3[2]);//5
+        System.out.println(arr2[2]);//33
+
+        //老师给的小练习，看看就好
+        int[] arr4 = new int[]{8, 2, 1, 0, 3};
+        int[] index = new int[]{2, 0, 3, 2, 4, 0, 1, 3, 2, 3, 3};
+        String tel = "";
+        for (int i = 0; i < index.length; i++) {
+            tel += arr4[index[i]];
+        }
+        System.out.println("联系方式：" + tel);
+
+        //输出对应的周几
+/*        String[] weeks = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};//这里绝对不可以写成加号
+        Scanner input = new Scanner(System.in);
+        System.out.println("周几（数字）：");
+        int whichDay;
+        whichDay = input.nextInt();
+        if (whichDay > 0 && whichDay < 8) {
+            System.out.println(weeks[whichDay-1]);
+        } else {
+            System.out.println("your type is wrong");
+        }
+        input.close;
+        */
+
+        //n个学生的成绩
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入有几个学生");
+        int peopleNum = input.nextInt();
+        int[] scores = new int[peopleNum];
+        int maxScore = 0;
+
+        System.out.println("请分别输入学生的成绩");
+        for (int i = 0; i < peopleNum; i++) {
+            scores[i] = input.nextInt();
+            if (scores[i] > maxScore) {
+                maxScore = scores[i];
+            }
+        }
+        System.out.println("一共有" + peopleNum + "名学生，成绩最高的为" + maxScore + "分。");
+        //分级
+        char grade = 0;
+        for (int i = 0; i < peopleNum; i++) {
+            if (scores[i] >= 90 & scores[i] <= 100) {
+                grade = 'A';
+            } else if (scores[i] >= 80) {
+                grade = 'B';
+            } else if (scores[i] >= 70) {
+                grade = 'C';
+            } else if (scores[i] >= 0 && scores[i] < 70) {
+                grade = 'D';
+            }
+            System.out.println("student" + i + " score is " + scores[i] + " grade is " + grade);
+        }
+        input.close();
 
 
     }
---
+++ Chapter05/src/multidimensionalArray.java
@@ -0,0 +1,6 @@
+public class multidimensionalArray {
+    public static void main(String[] args) {
+
+
+    }
+}
